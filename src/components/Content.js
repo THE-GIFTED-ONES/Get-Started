1153bda1a5caaec754f17e41102687d440475dcb
Udana D. Kodikara <chipseyofficial@gmail.com>
2023-01-06 18:44:13 +0530

    first commit

---
+++ src/components/Content.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+let date = new Date()
+const hour = date.getHours()
+
+export default function Content(props) {
+    return (
+        <div className={props.mode}>
+            <div className="gets--home">
+                <div className="content--background"></div>
+                <img src="gets5.png" alt="background" className="gets--back"></img>
+                <div className="devider"></div>
+                <div className="right--align">
+                    <div className="gets--content">
+                        <img src="logo.png" alt="logo" width="300px" className="logo"></img>
+                        <p className="gets--p">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi nihil expedita voluptate in perferendis culpa </p>
+                        <div className="gets--buttons">
+                            <a href="#">
+                                <button className="button--gs">GET STARTED</button>
+                            </a>
+                            
+                        </div>
+                        <div className="gets--buttons">
+                            <a href="#">
+                                <button className="button--si">LOG IN</button>
+                            </a>  
+                        </div>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    )  
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export default function Navbar(props) {
+    return (
+        <div className={props.mode}>
+            <nav>
+                <img src="logo.png" alt="logo" className="nav--logo"></img>
+                <ul className="nav--list">
+                    <li></li>
+                    <li>LOGIN</li>
+                    <li>ABOUT</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
